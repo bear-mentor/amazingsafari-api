a564d842df6bf5f8a5dff69f4476b4f5f87df54e
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-08 20:47:47 +0700

    feat: put more products to seed

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -1,8 +1,77 @@
-export const dataSeedProducts = [
+import { Product } from "../../src/generated/prisma";
+
+export type DataProduct = Omit<Product, "id" | "createdAt" | "updatedAt">;
+
+export const dataSeedProducts: DataProduct[] = [
   {
-    name: "Panda Cushion",
-    price: 50000,
-    description: "Simple cushion",
-    imageUrl: "https://safariwonders.com/wp-content/uploads/2023/06/h11xxx003305-head-panda-cushion-1686803573.jpg",
+    slug: "head-panda-cushion",
+    name: "Head Panda Cushion",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2023/06/h11xxx003305-head-panda-cushion-1686803573-480x480.jpg",
+    price: 115000,
+    description: "Mini panda head cushion bag",
+    // sku: "H11XXX003305",
+  },
+  {
+    slug: "big-head-cuddly-elephant",
+    name: "Big Head Cuddly Elephant",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2023/06/t01xxx000003-big-head-cuddly-elephant-1686803856-480x480.jpg",
+    price: 105000,
+    description: "Mini elephant plush toy",
+    // sku: "T01XXX000003",
+  },
+  {
+    slug: "lying-white-tiger",
+    name: "Lying White Tiger",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2023/06/b27xgi000010-lying-white-tiger-11-1686803201-480x480.jpg",
+    price: 215000,
+    description: "Small lying white tiger plush",
+    // sku: "B27XGI000010",
+  },
+  {
+    slug: "walking-rhino",
+    name: "Walking Rhino",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2023/06/b25xxx000018-walking-rhino-7x11-16283s-1686730601-480x480.jpg",
+    price: 125000,
+    description: "Walking rhino plush toy",
+    // sku: "B25XXX000018",
+  },
+  {
+    slug: "bali-starling",
+    name: "Bali Starling",
+    imageUrl: "https://safariwonders.com/wp-content/uploads/2023/06/t011xx000047-1686726398-480x480.jpg",
+    price: 155000,
+    description: "Bali myna bird plush",
+    // sku: "T011XX0000470",
+  },
+  {
+    slug: "lying-brown-tiger",
+    name: "Lying Brown Tiger",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2023/06/b27xgl000009-lying-brown-tiger-11-215.000-1687748170-480x480.jpg",
+    price: 215000,
+    description: "Lying brown tiger plush",
+    // sku: "B27XGL000009",
+  },
+  {
+    slug: "baby-polar-bear",
+    name: "Baby Polar Bear",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2023/06/b02xxx000002-baby-polar-bear-stuffed-1686801426-480x480.jpg",
+    price: 230000,
+    description: "Baby polar bear plush",
+    // sku: "B02XXX000002",
+  },
+  {
+    slug: "giraffe-stuffed",
+    name: "Giraffe Stuffed",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2023/06/b09xax000007-giraffe-stuffed-6-105.000-1687748005-480x480.jpg",
+    price: 105000,
+    description: "Giraffe stuffed plush",
+    // sku: "B09XAX000007",
   },
 ];
