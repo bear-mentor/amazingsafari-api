9544b42c101abedabc89041312d852726d51a194
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-18 21:35:30 +0700

    feat: user route and schema

---
+++ src/modules/user/route.ts
@@ -0,0 +1,62 @@
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import { UserSchema, UsersSchema, UsersIdSchema } from "./schema";
+import { prisma } from "../../lib/prisma";
+
+export const userRoute = new OpenAPIHono();
+
+userRoute.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    responses: {
+      200: {
+        content: { "application/json": { schema: UsersSchema } },
+        description: "Get all users",
+      },
+    },
+  }),
+  async (c) => {
+    const users = await prisma.user.findMany({
+      omit: {
+        email: true,
+      },
+    });
+
+    return c.json(users);
+  }
+);
+
+userRoute.openapi(
+  createRoute({
+    method: "get",
+    path: "/{id}",
+    request: {
+      params: UsersIdSchema,
+    },
+    responses: {
+      200: {
+        content: { "application/json": { schema: UserSchema } },
+        description: "Get all users",
+      },
+      404: {
+        description: "Not found",
+      },
+    },
+  }),
+  async (c) => {
+    const { id } = c.req.valid("param");
+
+    const user = await prisma.user.findUnique({
+      where: { id },
+      omit: {
+        email: true,
+      },
+    });
+
+    if (!user) {
+      return c.json({ message: "User not found" }, 404);
+    }
+
+    return c.json(user);
+  }
+);
